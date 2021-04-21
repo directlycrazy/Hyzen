8769f0dc0c0bc99d87905bd62b932ac4b96cf1d9
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2021-04-21 16:21:00 -0400

    update watch time

---
+++ assets/js/watch_time.js
@@ -0,0 +1,35 @@
+$('#player').ready(() => {
+	var query = new URLSearchParams(window.location.search).get('v');
+	var date = new Date();
+	var d = date.getTime();
+	if (localStorage.getItem('watch_date')) {
+		if (!isNaN(localStorage.getItem('watch_date'))) {
+			if ((d - localStorage.getItem('watch_date')) >= 2678400) {
+				localStorage.removeItem('watch_history');
+				localStorage.setItem('watch_date', d);
+			}
+		} else {
+			localStorage.setItem('watch_date', d);
+		}
+	} else {
+		localStorage.setItem('watch_date', d);
+	}
+	const update = (id, value) => {
+		var a = localStorage.getItem('watch_history');
+		if (!a) {
+			localStorage.setItem('watch_history', `{"${id}": ${value}}`);
+			return
+		} else {
+			var b = JSON.parse(a)
+			b[id] = value
+			localStorage.setItem('watch_history', JSON.stringify(b))
+			return
+		}
+	};
+	if (JSON.parse(localStorage.getItem('watch_history'))[query]) {
+		document.getElementById('player').currentTime = JSON.parse(localStorage.getItem('watch_history'))[query];
+	}
+	setInterval(() => {
+		return update(query, document.getElementById('player').currentTime);
+	}, 5000);
+})
