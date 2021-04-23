7a7a046e243ba310857d206fdd653661b85360dc
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2021-04-23 19:33:21 -0400

    proxy images by default

---
+++ src/routes/proxy.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const url = require('url');
+const https = require('https');
+
+const router = express.Router();
+
+router.get('/thumbnail', async (req, res) => {
+	if (req.query.q) {
+		try {
+			var parts = url.parse(req.query.q);
+			var proxy = https.request({
+				hostname: parts.hostname,
+				path: parts.path
+			}, function (response) {
+				res.writeHead(response.statusCode, response.headers)	
+				response.pipe(res, {
+					end: true
+				});
+			});
+		
+			req.pipe(proxy, {
+				end: true	
+			});
+		} catch (e) {
+			console.log(e)
+			return res.sendStatus(400)
+		}
+	} else {
+		return res.redirect('/');
+	}
+});
+
+module.exports = router;
