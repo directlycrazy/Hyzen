b119cf820f3d78795bac9c847519f58b21f7a0a6
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2021-04-29 14:48:15 -0400

    enhance autoplay

--- assets/js/autoplay.js
+++ assets/js/autoplay.js
@@ -2,9 +2,33 @@ $(document).ready(function () {
 	if (localStorage.getItem('autoplay') === 'true') {
 		$('#autoplay').prop("checked", true);
 		document.getElementById('player').addEventListener('ended', () => {
-			setTimeout(() => {
-				window.location.href = '/video?v=' + document.getElementById('recommendations').firstChild.nextSibling.id.split('_')[1];
-			}, 5000)
+			var i = 0;
+			var t = 8;
+			var scrolled = false;
+			var f = () => {
+				scrolled = false;
+				if (i < 8) {
+					document.getElementById('autoplaying').innerHTML = 'Playing in ' + t + '...';
+					i++;
+					return t = t - 1;
+				} else {
+					var id = document.getElementById('recommendations').firstChild.nextSibling.id;
+					return window.location.href = '/video?v=' + id.slice(id.split('_')[0].length + 1, 26);
+				}
+			};
+			var a = setInterval(f, 1000);
+			window.onscroll = function (e) {
+				if (scrolled === false) {
+					scrolled = true;
+					clearInterval(a);
+					document.getElementById('autoplaying').innerHTML = 'Playing in 8...';
+					setTimeout(() => {
+						t = 8;
+						i = 0;
+						return a = setInterval(f, 1000);
+					}, 2000);
+				}
+			};
 		});
 	}
 	$('#autoplay').click(function () {
