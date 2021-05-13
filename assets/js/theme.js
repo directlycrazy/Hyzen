959ecd1a475703535a368dc61bb086f65641f649
directlycrazy <33164679+directlycrazy@users.noreply.github.com>
2021-05-13 16:29:49 -0400

    ui improvements

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -4,9 +4,9 @@ $(document).ready(() => {
 	link.type = 'text/css';
 	document.getElementsByTagName('HEAD')[0].appendChild(link);
 	if (localStorage.getItem('theme') === 'dark') {
-		link.href = '/bootstrap/css/bootstrap-dark.min.css';
+		link.href = '/assets/dark.css';
 	} else {
-		link.href = '/bootstrap/css/bootstrap.min.css';
+		link.href = '/assets/light.css';
 	}
 	$('#toggle_night_mode').click(() => {
 		if (localStorage.getItem('theme') === 'dark') {
--- assets/js/watched.js
+++ assets/js/watched.js
@@ -2,7 +2,8 @@ $(document).ready(() => {
 	var b = Object.keys(JSON.parse(localStorage.getItem('watch_history')));
 	Array.from(document.getElementById('recommendations').children).forEach((a, i) => {
 		if (b.includes(a.id.replace('recommendation_', ''))) {
-			document.getElementById('thumbnail_parent_' + a.id.replace('recommendation_', '')).innerHTML += `<p style="font-weight:700;position:absolute;bottom:8px;right:16px;color:#fff;background:rgba(0,0,0,.7);border-radius:10%;padding:3px">Watched</p>`;
+			//document.getElementById('thumbnail_parent_' + a.id.replace('recommendation_', '')).style.display = 'flex'
+			//document.getElementById('thumbnail_parent_' + a.id.replace('recommendation_', '')).innerHTML += `<p style="font-weight:700;color:#fff;width:64.88px;height:30px;margin-left:auto;margin-top:auto;bottom:16px;right:16px;background:rgba(0,0,0,.7);border-radius:10%;padding:3px">Watched</p>`;
 		}
 	});
 })
